13e791acd2121eaab91a9d9697487de6a35b1e0b
PedrOliveiraM <pedropucmont@gmail.com>
2024-08-15 21:09:58 -0300

    chore: header update

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -35,7 +35,7 @@ const Header = () => {
             </SheetHeader>
             <div className="flex items-center gap-3 border-b border-solid py-5">
               <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6">
+                <Avatar className="h-10 w-10">
                   <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
                 </Avatar>
               </div>
